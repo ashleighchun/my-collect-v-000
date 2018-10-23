685260fcf851bd634bf2280f330aa5cab8c4b58c
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-23 02:24:15 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+my_collect(collection)
+  modified_collection=[]
+  i = 0
+  while i < collection.length
+    modified_collection << yield(collection[i])
+    i += 1
+  end
+  
+end
+
+
+
+
+#my_collect does not modify the original collection
+#yields correct element from collection 
+#returns new collection of modified elements
+
 
 
